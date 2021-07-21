4680c733bd9eec99ea47eadfd4cbbb9ce8660077
Shay Wacker <shay2442@yahoo.com>
2021-07-21 12:14:23 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+const newHeader = document.createElement('h1')
+document.body.appendChild(newHeader);
+newHeader.id = 'victory'
+victory.innerHTML = "<h1>Joe is the champion</h1>";
